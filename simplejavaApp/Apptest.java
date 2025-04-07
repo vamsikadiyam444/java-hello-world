f98c602e09d8344292589ed437665bd57ad32c52
vamsikadiyam444 <vamsikadiyam444@gmail.com>
2025-04-07 14:18:14 -0400

    Create Apptest.java

---
+++ simplejavaApp/Apptest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3), "2 + 3 should equal 5");
+    }
+}
