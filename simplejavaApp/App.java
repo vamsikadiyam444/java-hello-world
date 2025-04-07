d0d581ccee3f5b1a383848d01b9ec5b153340380
vamsikadiyam444 <vamsikadiyam444@gmail.com>
2025-04-07 14:17:40 -0400

    Create App.java

---
+++ simplejavaApp/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Simple Java App!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
